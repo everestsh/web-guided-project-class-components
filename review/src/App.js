e40244a46f35acbb1c2fc9e5f08ababbcd08afd2
sooof <sooof@me.com>
2021-11-29 15:57:18 -0800

    handleSingleClickEvent && handleDoubleClickEvent

--- review/src/App.js
+++ review/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { render } from "react-dom";
 
-const App = () => {
-  return (
-    <div>
-      <h1>Hello Warren</h1>
-    </div>
-  );
+class App extends React.Component {
+  handleSingleClickEvent = () => alert("Single Click Event Triggered")
+  handleDoubleClickEvent = () => alert("Double Click Event Triggered")
+  render(){
+    return (
+      <div>
+        <h1>Event Handles</h1>
+        <button onClick={this.handleSingleClickEvent}>Single Click Handler</button>
+        <hr/>
+        <button onClick={this.handleDoubleClickEvent}>Double Click Handler</button>     
+        <hr/>
+        <div>Mouse Enter</div>
+        <hr/>
+        <div>Mouse Leave</div>
+        <hr/>
+        <input type="text" placeholder="Change me"/>
+      </div>
+    );
+  }
 };
 
 export default App;
