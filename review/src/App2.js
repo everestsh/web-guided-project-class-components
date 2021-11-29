46d0335c40394b4a1bce757463a923fd1785afdc
sooof <sooof@me.com>
2021-11-29 10:03:17 -0800

    use Class can no constructor

--- review/src/App2.js
+++ review/src/App2.js
@@ -3,18 +3,18 @@ import React, { useState } from "react";
 
 
 class App2 extends React.Component {
-  constructor(){
-    super();
-    this.state ={
-      name : "Warren",
-      age : 33
-    }
+  // constructor(){
+  //   super();
+  // }
+  state ={
+    name : "Warren",
+    age : 33
   }
 
  render () {
   return (
     <div>
-      <h1>Hello {this.state.name}</h1>
+      <h1>Hello {this.state.name}, You are {this.state.age} years old.</h1>
     </div>
   )
  }
