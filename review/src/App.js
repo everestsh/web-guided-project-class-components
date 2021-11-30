9be8359d77ca2234c3ac7b7d8f45bfc99c8a4afc
sooof <sooof@me.com>
2021-11-29 16:15:52 -0800

    save event.target.value to this.state.message

--- review/src/App.js
+++ review/src/App.js
@@ -2,17 +2,27 @@ import React from "react";
 import { render } from "react-dom";
 
 class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      message: ''
+    }
+  }
   handleSingleClickEvent = () => alert("Single Click Event Triggered")
   handleDoubleClickEvent = () => alert("Double Click Event Triggered")
   handleMouseEnter = () => alert("Mouse Enter Event Triggered")
   handleMouseLeave = () => alert("Mouse Leave Event Triggered")
   handleInputChange = event => {
     console.log(event.target.value)
+    this.setState({
+      ...this.state,
+      message: event.target.value
+    })
   }
   render(){
     return (
       <div>
-        <h1>Event Handles</h1>
+        <h1>Event Handles {this.state.message}</h1>
         <button onClick={this.handleSingleClickEvent}>Single Click Handler</button>
         <hr/>
         <button onDoubleClick={this.handleDoubleClickEvent}>Double Click Handler</button>     
