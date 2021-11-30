27d9ef028733d4571afde7a4fe75cd30343c0b0a
sooof <sooof@me.com>
2021-11-29 18:08:12 -0800

    add class Canine extends Animal

--- review/src/App.js
+++ review/src/App.js
@@ -16,14 +16,47 @@ class Animal {
     console.log(`${this.name} is talking.`)
   }
 }
+class Canine extends Animal {
+   constructor(name, age, barkiness){
+      super(name, age, "canine")
+      this.barkiness = barkiness
+   }
+   dogStuff = () => {
+    console.log(`${this.name} is doing dog stuff`)
+  }
+  talk = () => {
+    // console.log(`${this.name} is talking.`)
+    if(this.barkiness >5){
+      console.log("BARRRK!")
+    }else{
+      console.log("Bark!") 
+    }
+  }
+}
 
 
 const fido = new Animal("fido", 2, "canine ")
 fido.eat()
 fido.describe()
 
+const po = new Animal("po", 8, "feline ")
+po.eat()
+po.describe()
+
+const greg = new Animal("greg", 12, "tortose")
+greg.eat()
+greg .describe()
+
+
+const grover = new Canine("grover", 4, 6)
+grover.eat()
+grover.describe()
+grover.talk()
+grover.dogStuff()
+
+
 const App = () => {
-  return (
+  return (  
     <div>
       <h1>Hello Warren</h1>
     </div>
