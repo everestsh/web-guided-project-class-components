e1794df377c52fd68cea4a0d43d2d487bb9434ff
sooof <sooof@me.com>
2021-11-29 16:04:54 -0800

    onMouseEnter && onMouseLeave

--- review/src/App.js
+++ review/src/App.js
@@ -4,17 +4,19 @@ import { render } from "react-dom";
 class App extends React.Component {
   handleSingleClickEvent = () => alert("Single Click Event Triggered")
   handleDoubleClickEvent = () => alert("Double Click Event Triggered")
+  handleMouseEnter = () => alert("Mouse Enter Event Triggered")
+  handleMouseLeave = () => alert("Mouse Leave Event Triggered")
   render(){
     return (
       <div>
         <h1>Event Handles</h1>
         <button onClick={this.handleSingleClickEvent}>Single Click Handler</button>
         <hr/>
-        <button onClick={this.handleDoubleClickEvent}>Double Click Handler</button>     
+        <button onDoubleClick={this.handleDoubleClickEvent}>Double Click Handler</button>     
         <hr/>
-        <div>Mouse Enter</div>
+        <div onMouseEnter={this.handleMouseEnter}>Mouse Enter</div>
         <hr/>
-        <div>Mouse Leave</div>
+        <div onMouseLeave={this.handleMouseLeave}>Mouse Leave</div>
         <hr/>
         <input type="text" placeholder="Change me"/>
       </div>
