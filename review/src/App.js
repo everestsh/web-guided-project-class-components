9f62c9d405e23eb0898fa7020213c7655b4c481b
sooof <sooof@me.com>
2021-11-29 17:33:58 -0800

    add class Animal

--- review/src/App.js
+++ review/src/App.js
@@ -1,5 +1,27 @@
 import React from "react";
 
+class Animal {
+  constructor(name, age, species){
+    this.name = name
+    this.age = age
+    this.species = species
+  }
+  eat = () => {
+    console.log(`${this.name} is eating.`)
+  }
+  describe = () => {
+    console.log(`${this.name} is ${this.species}.`)
+  }
+  talk = () => {
+    console.log(`${this.name} is talking.`)
+  }
+}
+
+
+const fido = new Animal("fido", 2, "canine ")
+fido.eat()
+fido.describe()
+
 const App = () => {
   return (
     <div>
